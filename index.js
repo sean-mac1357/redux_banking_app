81129528fe36843a9c281a3541adba7500f79e97
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-02-23 14:56:56 -0500

    added in html and buttons in order to increment or decrement the balance

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+
+const { createStore } = Redux;
 
 console.log('App Started!');
 
@@ -33,12 +35,28 @@ const account = (state=defaultState, action) => {
     }
 }
 
-const store = createStore(account);
+const store = createStore(
+    account,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
 store.subscribe(() => {
     console.log('subscribing to state changes....');
     const state = store.getState();
     console.log("the state is:", state);
+    const balance = document.querySelector('#balance');
+    balance.innerHTML = state.balance;
 });
 
-store.dispatch(actionIncrement);
+const incrementButton = document.querySelector('#add');
+const decrementButton = document.querySelector('#subtract');
+
+incrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    store.dispatch(actionIncrement);
+});
+
+decrementButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    store.dispatch(actionDecrement);
+});
